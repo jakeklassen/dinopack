094ee8332bf9faa24ee93f3bd2a421ce0f651276
Jake Klassen <jklassendev@gmail.com>
2022-11-03 09:19:08 -0400

    using zod for schema

--- api/binpacker.ts
+++ api/binpacker.ts
@@ -1,32 +1,59 @@
 import { serve } from "std/http/server.ts";
+import { z } from "zod";
 import { Bin } from "../lib/3d/bin.ts";
 import { Item } from "../lib/3d/item.ts";
 import { Packer } from "../lib/3d/packer.ts";
+
+const packSchema = z.object({
+  bins: z.array(
+    z.object({
+      name: z.string(),
+      width: z.number(),
+      height: z.number(),
+      depth: z.number(),
+      maxWeight: z.number(),
+    }),
+  ).nonempty().transform((bins) =>
+    bins.map((bin) =>
+      new Bin(bin.name, bin.width, bin.height, bin.depth, bin.maxWeight)
+    )
+  ),
+  items: z.array(
+    z.object({
+      name: z.string(),
+      width: z.number(),
+      height: z.number(),
+      depth: z.number(),
+      weight: z.number(),
+    }),
+  ).nonempty().transform((items) =>
+    items.map((item) =>
+      new Item(item.name, item.width, item.height, item.depth, item.weight)
+    )
+  ),
+});
 
 serve(async (request) => {
   if (request.method !== "POST") {
     return new Response("Method not allowed", { status: 405 });
   }
 
-  const { bins: binsJSON, items: itemsJSON } = await request.json();
+  const payload = await request.json();
 
-  if (!Array.isArray(binsJSON) || !Array.isArray(itemsJSON)) {
-    return new Response("Invalid request body", { status: 400 });
+  try {
+    const { bins, items } = packSchema.parse(payload);
+
+    const packer = new Packer(
+      bins,
+      items,
+    );
+
+    const bin = packer.pack();
+
+    return new Response(JSON.stringify(bin), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(error.message, { status: 400 });
   }
-
-  const bins = binsJSON.map((bin) =>
-    new Bin(bin.name, bin.width, bin.height, bin.depth, bin.maxWeight)
-  );
-
-  const items = itemsJSON.map((item) =>
-    new Item(item.name, item.width, item.height, item.depth, item.weight)
-  );
-
-  const packer = new Packer(bins, items);
-
-  const bin = packer.pack();
-
-  return new Response(JSON.stringify(bin), {
-    status: 201,
-  });
 });
